374042d7c5b6245a0131b8e595e9f54064519a86
Hrutujaa21 <kamthe.hrutuja@gmail.com>
2025-01-10 22:50:37 +0530

    Designed UI

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Abcom on 10/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+
+}
+
