b55474f16af8db3761d4061500948bc319896788
Dipak Patil <dipakpatil@Dipaks-MacBook-Air.local>
2025-01-13 23:40:01 +0530

    Dealing with decimal place and code refactoring

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     var isFinishedTyping: Bool = true
+    
+    private var calculator = CalculatorLogic()
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,56 +33,38 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-        
+        ///What should happen when a non-number button is pressed
         isFinishedTyping = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
+        calculator.setNumber(displayValue)
         
         if let calcMethod = sender.currentTitle {
-            if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
-            } else if calcMethod == "AC" {
-                displayLabel.text = "0"
-            } else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else {
+                fatalError("The results of the calculation is nil.")
             }
+            displayValue = result
         }
-    
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-        
+        ///What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle {
-            
             if isFinishedTyping {
                 displayLabel.text = numValue
                 isFinishedTyping = false
             } else {
                 if numValue == "." {
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert display label text to a Double")
-                    }
-                    
-                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    let isInt = floor(displayValue) == displayValue
                     
                     if !isInt {
                         return
                     }
                 }
-                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
     
     }
-
-
 }
 
